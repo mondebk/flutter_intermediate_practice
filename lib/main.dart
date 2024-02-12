537166a350a0238500737c12f0e1b5d9355bf687
Monde Keikelame <cptmonde@gmail.com>
2024-02-12 07:51:30 +0200

    merge: revert main back to default

--- lib/main.dart
+++ lib/main.dart
@@ -33,52 +33,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-    for (int i = 0; i < 5; i++) {
-      Widget child = _newItem(i);
-      _list.add(child);
-    }
-  }
-
-  int counter = 0;
-
-  /// List<Widget> = new List<Widget>() is no longer supported as of Dart3
-  ///   The method below is required in order to initialise a dynamically sized list.
-   List<Widget> _list = <Widget>[];
-
-  void _onClicked() {
-    Widget child = _newItem(counter);
-    setState(() => _list.add(child));
-  }
-
-  Widget _newItem(int i) {
-    Key key = Key('item_$i');
-    Container child = Container(
-        key: key,
-        padding: const EdgeInsets.all(10.0),
-        child: Chip(
-          label: Text('$i Name here'),
-          deleteButtonTooltipMessage: 'Delete',
-          deleteIconColor: Colors.red,
-          onDeleted: () => removeItem(key),
-          avatar: CircleAvatar(
-              backgroundColor: Colors.grey.shade800, child: Text(i.toString())),
-        ));
-    counter++;
-    return child;
-  }
-
-  void removeItem(Key key) {
-    for (int i = 0; i < _list.length; i++) {
-      Widget child = _list.elementAt(i);
-      if (child.key == key) {
-        setState(() => _list.removeAt(i));
-        print('Removing ${key.toString()}');
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -87,12 +41,12 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () => _onClicked(), child: const Icon(Icons.add)),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: _list
+          children: <Widget>[
+             Text('Add widgets here'),
+          ]
         ),
       ),
     );
